57e4926c02a4086ded1cd7a63d2a36385f2ba7f7
chriskoutroulis <chriskoutroulis@gmail.com>
2016-07-13 16:23:56 +0300

    Removed a //TODO and corrected a misspelled argument name.

--- app/src/main/java/ais/koutroulis/gr/client/MoodleClient.java
+++ app/src/main/java/ais/koutroulis/gr/client/MoodleClient.java
@@ -7,18 +7,13 @@ import java.io.IOException;
  */
 public interface MoodleClient {
 
-    //TODO Create 2 objects to hold the parameters for the last 4 methods.
-    //It should probably hold only the common to all parameters (String script, String format, String token, String function).
-    //The login method is probably ok, with only 4 String parameters.
-    //Maybe the rest of the arguments could be inside a Map<String,String> .. Maybe
-
     public <T> T login(String script, String loginService, String username, String password) throws IOException;
 
     public <T> T getCourses(MoodleUrlCommonParts urlCommonParts) throws IOException;
 
     public <T> T getUserDetails(MoodleUrlCommonParts urlCommonParts, String byField, String fieldValue) throws IOException;
 
-    public <T> T getMessages(MoodleUrlCommonParts urlCommonPartsn, String markAsReadFunction, String sentToId,
+    public <T> T getMessages(MoodleUrlCommonParts urlCommonParts, String markAsReadFunction, String sentToId,
                              String sentFromId, String oneForReadZeroForUnread, String timeReadInMillis) throws IOException;
 
     public <T> T markAsReadMessage(MoodleUrlCommonParts urlCommonParts, String unreadMessageId,
